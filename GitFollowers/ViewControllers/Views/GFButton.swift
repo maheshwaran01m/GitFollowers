14ae8f5edb2f9e87380ea0357cb3fdf2c772669f
MAHESHWARAN <maheshwaran99m@gmail.com>
2023-12-30 16:59:20 +0530

    First Commit

---
+++ GitFollowers/ViewControllers/Views/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  GitFollowers
+//
+//  Created by MAHESHWARAN on 30/12/23.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setup()
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    setup()
+  }
+  
+  init(_ title: String, backgroundColor: UIColor) {
+    super.init(frame: .zero)
+    self.backgroundColor = backgroundColor
+    setTitle(title, for: .normal)
+    setup()
+  }
+  
+  private func setup() {
+    layer.cornerRadius = 10
+    titleLabel?.textColor = .label
+    titleLabel?.font = .preferredFont(forTextStyle: .headline)
+    translatesAutoresizingMaskIntoConstraints = false
+  }
+}
