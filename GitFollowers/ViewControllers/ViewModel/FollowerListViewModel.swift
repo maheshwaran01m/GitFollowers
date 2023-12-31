8057848914c350db9a2a4e6cd05bf4bd2cb4e359
MAHESHWARAN <maheshwaran99m@gmail.com>
2023-12-31 07:52:22 +0530

    FollowerListViewModel  - Handled more followers fetch

--- GitFollowers/ViewControllers/ViewModel/FollowerListViewModel.swift
+++ GitFollowers/ViewControllers/ViewModel/FollowerListViewModel.swift
@@ -16,6 +16,9 @@ class FollowerListViewModel {
   var userName: String
   var followers: [Follower] = []
   
+  private var page = 1
+  private var hasMoreFollowers = true
+  
   weak var alertDelegate: AlertProtocol?
   weak var delegate: FollowerProtocol?
   
@@ -23,24 +26,43 @@ class FollowerListViewModel {
   
   init(_ userName: String) {
     self.userName = userName
-    getFollowers(for: userName)
+    getFollowers(for: userName, page: page)
   }
   
-  func getFollowers(for userName: String) {
-    manager.getFollowers(userName, page: 1) { [weak self] result in
+  func getFollowers(for userName: String, page: Int) {
+    manager.getFollowers(userName, page: page) { [weak self] result in
       guard let self else { return }
       
       switch result {
       case .success(let followers):
-        DispatchQueue.main.async {
-          self.followers = followers
-          self.delegate?.updateUI()
-        }
+        self.add(followers)
+        
       case .failure(let error):
         self.alertDelegate?.showAlert(.init("Bad Error", message: error.localizedDescription))
         print(error.localizedDescription)
       }
     }
+  }
+  
+  private func add(_ followers: [Follower]) {
+    DispatchQueue.main.async { [weak self] in
+      guard let self else { return }
+      
+      self.hasMoreFollowers = !(followers.count < 100)
+      self.followers.append(contentsOf: followers)
+      self.delegate?.updateUI()
+    }
+  }
+  
+  func updateFollowers(_ scrollView: UIScrollView) {
+    guard hasMoreFollowers else { return }
+    let offsetY = scrollView.contentOffset.y
+    let contentHeight = scrollView.contentSize.height
+    let height = scrollView.frame.size.height
+    
+    guard offsetY > contentHeight - height else { return }
+    page += 1
+    getFollowers(for: userName, page: page)
   }
 }
 
